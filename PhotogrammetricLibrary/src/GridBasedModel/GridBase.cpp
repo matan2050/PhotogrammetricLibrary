367dfcc11493c13bc5812303b6491111b2c9a9c1
Matan <matan2050@gmail.com>
2020-06-07 22:25:35 +0300

    removed old folder tree structure

--- PhotogrammetricLibrary/src/GridBasedModel/GridBase.cpp
+++
@@ -1,9 +0,0 @@
-#include "GridBase.h"
-
-GridBasedModels::Grid::Grid(float _start_x, float _start_y, float _step_x, float _step_y)
-{
-    start_x = _start_x;
-    start_y = _start_y;
-    step_x = _step_x;
-    step_y = _step_y;
-}
--- PhotogrammetricLibrary/src/OrthophotoModels/Orthophoto.cpp
+++
@@ -1,5 +0,0 @@
-#include "Orthophoto.h"
-
-OrthophotoModels::Orthophoto::Orthophoto(float _start_x, float _start_y, float _step_x, float _step_y) :
-    GridBasedModels::Grid(_start_x, _start_y, _step_x, _step_y)
-{}
