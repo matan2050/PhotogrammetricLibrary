c6bce136904475d2f26651e5c1d7bc98e8206bb4
Matan <matan2050@gmail.com>
2020-06-07 21:38:32 +0300

    fixed folder tree

---
+++ src/GridBasedModel/GridBase.cpp
@@ -0,0 +1,9 @@
+#include "GridBase.h"
+
+GridBasedModels::Grid::Grid(float _start_x, float _start_y, float _step_x, float _step_y)
+{
+    start_x = _start_x;
+    start_y = _start_y;
+    step_x = _step_x;
+    step_y = _step_y;
+}
